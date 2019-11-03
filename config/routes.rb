c967627ec6c2336ae35fcff6dcc6646b64459c9c
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-11-03 17:10:34 +0100

    altri fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :events
   root 'events#map'
-  get 'wiki', to: 'events#wiki'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
